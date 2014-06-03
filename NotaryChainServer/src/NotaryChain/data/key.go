69e1e770689d9f3f265dab7504df5cf23442916e
Ethan Reesor <ethan.reesor@firelizzard.com>
2014-06-02 22:35:54 -0500

    Implementing ECDSA and RSA Keys and Signatures

--- NotaryChainServer/src/NotaryChain/data/key.go
+++ NotaryChainServer/src/NotaryChain/data/key.go
@@ -2,20 +2,66 @@ package data
 
 import (
 	"hash"
+	"crypto/rsa"
+	"crypto/ecdsa"
+	"crypto/elliptic"
 )
 
 const (
 	BadKeyType		= -1
-	ECCKeyType		=  0
-	RSAKeyType		=  1
+	ECDSAPubKeyType	=  0
+	ECDSAPrivKeyType=  1
+	RSAPubKeyType	=  2
+	RSAPrivKeyType	=  3
 )
 
 type Key struct {
 	KeyType			int8
-	KeyData			[]byte
+}
+
+type ECDSAPubKey struct {
+	Key
+	ecdsa.PublicKey
+}
+
+type ECDSAPrivKey struct {
+	Key
+	ecdsa.PrivateKey
+}
+
+type RSAPubKey struct {
+	Key
+	rsa.PublicKey
+}
+
+type RSAPrivKey struct {
+	Key
+	rsa.PrivateKey
 }
 
 func (k *Key) writeToHash(h hash.Hash) (err error) {
-	_, err = h.Write(k.KeyData)
+	return nil
+}
+
+func (k *ECDSAPubKey) writeToHash(h hash.Hash) (err error) {
+	if _, err = h.Write(elliptic.Marshal(k, k.X, k.Y)); err != nil {
+		return err
+	}
+	
+	if _, err = h.Write(k.X.Bytes()); err != nil {
+		return err
+	}
+	
+	_, err = h.Write(k.Y.Bytes())
+	return err
+}
+
+func (k *ECDSAPrivKey) writeToHash(h hash.Hash) (err error) {
+	pub := &ECDSAPubKey{k.Key, k.PrivateKey.PublicKey}
+	if err = pub.writeToHash(h); err != nil {
+		return err
+	}
+	
+	_, err = h.Write(k.D.Bytes())
 	return err
 }
