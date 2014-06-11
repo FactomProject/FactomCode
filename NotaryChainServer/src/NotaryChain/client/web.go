ed1eb3fd2e98737755abbfbf06a31441112d981a
Ethan Reesor <ethan.reesor@firelizzard.com>
2014-06-11 18:24:54 -0500

    Saving work

--- NotaryChainServer/src/NotaryChain/client/web.go
+++ NotaryChainServer/src/NotaryChain/client/web.go
@@ -2,96 +2,147 @@ package main
 
 import (
 	"flag"
-	"net/http"
-	"text/template"
+	"fmt"
 	"strconv"
-	"sync"
-	"time"
-	"encoding/json"
-	ncdata "NotaryChain/data"
-	ncrest "NotaryChain/rest"
+	
+	"io/ioutil"
+	"path/filepath"
+	"text/template"
+	
+	"github.com/hoisie/web"
+	md "github.com/russross/blackfriday"
+	
+	"NotaryChain/restapi"
+	//"NotaryChain/pagebuilder"
+	"github.com/firelizzard18/dynrsrc"
 )
 
+var mdrdr md.Renderer
+var mdext int
+var mainTmpl *template.Template
 var portNumber *int = flag.Int("p", 8087, "Set the port to listen on")
-var homeTmpl, editJSONTmpl *template.Template
-var entry *ncdata.PlainEntry
 
-func init() {
-	var err error
-	
-	entry = new(ncdata.PlainEntry)
-	entry.StructuredData = make([]byte, 0)
-	entry.Signatures = make([]*ncdata.Signature, 0)
-	
-	homeTmpl, err = template.ParseFiles("test/client/home.gwp");
-	if err != nil { panic(err) }
-	
-	editJSONTmpl, err = template.ParseFiles("test/client/json.gwp");
-	if err != nil { panic(err) }
+func watchError(err error) {
+	panic(err)
 }
 
-var blockMutex = &sync.Mutex{}
-
-func main() {
-	http.HandleFunc("/", serveTemplate(runHome))
-	http.HandleFunc("/json", serveTemplate(runEditJSON))
-	http.ListenAndServe(":" + strconv.Itoa(*portNumber), nil)
+func readError(err error) {
+	fmt.Println("error: ", err)
 }
 
-func serveTemplate(run func(w http.ResponseWriter, r *http.Request) *ncrest.Error) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if err := run(w, r); err != nil {
-			data, r := ncrest.Marshal(err, "html")
-			if r != nil {
-				err = r
-			}
-			w.WriteHeader(err.HTTPCode)
-			w.Write(data)
+func buildTemplateTree() (main *template.Template, err error) {
+	main, err = template.ParseFiles("app/client/page.gwp")
+	if err != nil { return }
+	
+	_, err = main.ParseGlob("app/client/*.gwp")
+	if err != nil { return }
+	
+	matches, err := filepath.Glob("app/client/*.md")
+	if err != nil { return }
+	
+	err = parseMarkdownTemplates(main, matches...)
+	return
+}
+
+func parseMarkdownTemplates(t *template.Template, filenames...string) error {
+	for _, filename := range filenames {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil { return err }
+		
+		data = md.Markdown(data, mdrdr, mdext)
+		
+		_, err = t.New(filepath.Base(filename)).Parse(string(data))
+		if err != nil { return err }
+	}
+	
+	return nil
+}
+
+func initTemplate(load func(string) error, filename string, markdown bool) {
+	handler := func(data []byte) {
+		if markdown {
+			data = md.Markdown(data, mdrdr, mdext)
 		}
 		
-		w.Write([]byte("\n\n"))
-	}
-}
-
-func runHome(w http.ResponseWriter, r *http.Request) *ncrest.Error {
-	if r.URL.Path != "/" {
-		w.WriteHeader(404)
-		return nil
-	}
-	
-	data := []byte(r.Form.Get("data"))
-	
-	if len(data) > 0 {
-		err := json.Unmarshal(data, entry)
+		err := load(string(data))
+		
 		if err != nil {
-			return ncrest.CreateError(ncrest.ErrorJSONUnmarshal, err.Error())
+			panic(err)
 		}
 	}
 	
-	entry.TimeStamp = time.Now().Unix()
+	err := dynrsrc.CreateDynamicResource(filename, handler)
 	
-	data, err := ncrest.Marshal(entry, "json")
-	if err != nil { return nil }
-	
-	if err := homeTmpl.Execute(w, string(data)); err != nil {
-		w.WriteHeader(500)
+	if err != nil {
+		panic(err)
 	}
-	
-	return nil
 }
 
-func runEditJSON(w http.ResponseWriter, r *http.Request) *ncrest.Error {
-	if r.URL.Path != "/json" {
-		w.WriteHeader(404)
-		return nil
+func main() {
+	var err error
+	
+	err = dynrsrc.Start(watchError, readError)
+	if err != nil { panic(err) }
+	
+	err = dynrsrc.CreateDynamicResource("app/client", func([]byte) {
+		t, err := buildTemplateTree()
+		if err != nil { readError(err) }
+		
+		mainTmpl = t
+	})
+	if err != nil { panic(err) }
+	
+	//pagebuilder.Match(`/(home)?`, "home", handleHome, "GET", "POST")
+	//pagebuilder.Match(`/entries/(\d+)`, "entry", handleEntry, "GET")
+	
+	//pagebuilder.Server.Config.StaticDir = "app/client/static"
+	
+	defer func() {
+		dynrsrc.Stop()
+	}()
+	
+	//pagebuilder.Server.Run("localhost:8087")
+}
+
+func handleHome(ctx *web.Context, matches []string) map[string]string {
+	switch ctx.Params["format"] {
+	case "json":
+		unmarshalEntryFromJSON([]byte(ctx.Params["data"]))
+		
+	case "ecdsa":
 	}
 	
-	data, err := ncrest.Marshal(entry, "json")
-	if err != nil { return nil }
+	updateEntryTimeStamp()
 	
-	if err := editJSONTmpl.Execute(w, string(data)); err != nil {
-		w.WriteHeader(500)
+	data, err := marshalEntryToJSON()
+	if err != nil { return handleError(err) }
+	
+	return map[string]string {
+		"Title": "Home",
+		"EntryJSON": string(data),
+		"OnLoad": "",
 	}
+}
+
+func handleEntry(ctx *web.Context, matches []string) map[string]string {
+	data, err := marshalEntryToJSON()
+	if err != nil { return handleError(err) }
 	
-	return nil
+	return map[string]string {
+		"Title": "Edit Entry " + matches[0],
+		"EntryJSON": string(data),
+		"OnLoad": "",
+	}
+}
+
+func handleError(err *restapi.Error) map[string]string {
+	data, r := restapi.Marshal(err, "html")
+	if r != nil { err = r }
+	
+	return map[string]string {
+		"Title": "Error",
+		"HTTPCode": strconv.Itoa(err.HTTPCode),
+		"Content": string(data),
+		"OnLoad": "",
+	}
 }
