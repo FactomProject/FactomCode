2b06cf694b1228eee7a22a1d7bea4127166f7c0c
Ethan Reesor <ethan.reesor@firelizzard.com>
2014-07-01 13:19:19 -0500

    Rearanging things

--- notaryapi/coding.go
+++ notaryapi/coding.go
@@ -27,52 +27,58 @@ func NewDecoding(decoding gocoding.Decoding) gocoding.Decoding {
 			return decoding(unmarshaller, theType)
 		}
 		
-		decoder := (&ECDSASignature{}).ElementDecoding(unmarshaller, theType)
+		return SignatureDecoding(unmarshaller, theType)
+	}
+}
+
+func SignatureDecoding(unmarshaller gocoding.Unmarshaller, theType reflect.Type) gocoding.Decoder {
+	decoders := []gocoding.Decoder{
+		(&ECDSASignature{}).ElementDecoding(unmarshaller, theType),
+	}
+	
+	return func(scratch [64]byte, scanner gocoding.Scanner, value reflect.Value) {
+		if scanner.Peek() == gocoding.ScannedLiteralBegin {
+			null := scanner.NextValue()
+			if null.IsValid() && null.IsNil() {
+				value.Set(reflect.Zero(theType))
+				return
+			}
+		}
+	
+		if !gocoding.PeekCheck(scanner, gocoding.ScannedStructBegin, gocoding.ScannedMapBegin) { return }
 		
-		return func(scratch [64]byte, scanner gocoding.Scanner, value reflect.Value) {
-			if scanner.Peek() == gocoding.ScannedLiteralBegin {
-				null := scanner.NextValue()
-				if null.IsValid() && null.IsNil() {
-					value.Set(reflect.Zero(theType))
-					return
-				}
-			}
+		// get the next code, should have at least a type key
+		code := scanner.Continue()
+		if code != gocoding.ScannedKeyBegin {
+			gocoding.PeekCheck(scanner, gocoding.ScannedKeyBegin)
+			return
+		}
 		
-			if !gocoding.PeekCheck(scanner, gocoding.ScannedStructBegin, gocoding.ScannedMapBegin) { return }
-			
-			// get the next code, should have at least a type key
-			code := scanner.Continue()
-			if code != gocoding.ScannedKeyBegin {
-				gocoding.PeekCheck(scanner, gocoding.ScannedKeyBegin)
-				return
-			}
-			
-			// get the key
-			key := scanner.NextValue()
-			if key.Kind() != reflect.String {
-				scanner.Error(gocoding.ErrorPrint("Decoding", "Invalid key type %s", key.Type().String()))
-				return
-			}
-			if str := strings.ToLower(key.String()); str != `type` {
-				scanner.Error(gocoding.ErrorPrintf("Decoding", "Decoding signature: expected 'type', got '%s'", str))
-				return
-			}
-			
-			scanner.Continue()
-			_type := scanner.NextValue()
-			switch _type.Kind() {
-			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
-				 reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-				 
-			default:
-				scanner.Error(gocoding.ErrorPrint("Decoding", "Invalid type code type %s", _type.Type().String()))
-			}
-			
-			switch _type.Int() {
-			case ECDSASignatureType:
-				value.Set(reflect.ValueOf(&ECDSASignature{}))
-				decoder(scratch, scanner, value)
-			}
+		// get the key
+		key := scanner.NextValue()
+		if key.Kind() != reflect.String {
+			scanner.Error(gocoding.ErrorPrint("Decoding", "Invalid key type %s", key.Type().String()))
+			return
+		}
+		if str := strings.ToLower(key.String()); str != `type` {
+			scanner.Error(gocoding.ErrorPrintf("Decoding", "Decoding signature: expected 'type', got '%s'", str))
+			return
+		}
+		
+		scanner.Continue()
+		_type := scanner.NextValue()
+		switch _type.Kind() {
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+			 reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			 
+		default:
+			scanner.Error(gocoding.ErrorPrint("Decoding", "Invalid type code type %s", _type.Type().String()))
+		}
+		
+		switch _type.Int() {
+		case ECDSASignatureType:
+			value.Set(reflect.ValueOf(&ECDSASignature{}))
+			decoders[0](scratch, scanner, value)
 		}
 	}
 }
