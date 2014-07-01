00c31a5bd0ac25d240e7eb714f428255ef0f64a4
Paul Snow <paulsn@DTRules.com>
2014-06-30 20:53:11 -0500

    integration

--- restapi/main.go
+++ restapi/main.go
@@ -1,4 +1,4 @@
-package main 
+package main
 
 import (
 	"bytes"
@@ -6,21 +6,29 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"github.com/NotaryChains/NotaryChainCode/notaryapi"
+	"github.com/conformal/btcrpcclient"
+	"github.com/conformal/btcutil"
+	"github.com/conformal/btcwire"
 	"github.com/firelizzard18/dynrsrc"
 	"github.com/firelizzard18/gobundle"
 	"github.com/firelizzard18/gocoding"
 	"github.com/firelizzard18/gocoding/json"
-	"github.com/NotaryChains/NotaryChainCode/notaryapi"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"path/filepath"
 	"reflect"
-	"time"
 	"strconv"
 	"strings"
 	"sync"
+	"time"
+	"log"
 )
+
+var client *btcrpcclient.Client
+var currentAddr *btcutil.Address
+var balance int64
 
 var portNumber = flag.Int("p", 8083, "Set the port to listen on")
 var blocks []*notaryapi.Block
@@ -37,19 +45,25 @@ func readError(err error) {
 
 func initWithBinary() {
 	matches, err := filepath.Glob(gobundle.DataFile("store.*.block"))
-	if err != nil { panic(err) }
-	
+	if err != nil {
+		panic(err)
+	}
+
 	blocks = make([]*notaryapi.Block, len(matches))
-	
+
 	num := 0
 	for _, match := range matches {
 		data, err := ioutil.ReadFile(match)
-		if err != nil { panic(err) }
-		
+		if err != nil {
+			panic(err)
+		}
+
 		block := new(notaryapi.Block)
 		err = block.UnmarshalBinary(data)
-		if err != nil { panic(err) }
-		
+		if err != nil {
+			panic(err)
+		}
+
 		blocks[num] = block
 		num++
 	}
@@ -58,30 +72,24 @@ func initWithBinary() {
 func init() {
 	gobundle.Setup.Application.Name = "NotaryChains/restapi"
 	gobundle.Init()
-	
+
 	dynrsrc.Start(watchError, readError)
 	notaryapi.StartDynamic(gobundle.DataFile("html.gwp"), readError)
-	
+
 	initWithBinary()
-	
+
 	fmt.Println("Loaded", len(blocks), "blocks")
-	
+
 	for i := 0; i < len(blocks); i = i + 1 {
 		if uint64(i) != blocks[i].BlockID {
 			panic(errors.New("BlockID does not equal index"))
 		}
 	}
 	notaryapi.UpdateNextBlockID(uint64(len(blocks)))
-	
+
 	tickers[0] = time.NewTicker(time.Minute * 5)
 	tickers[1] = time.NewTicker(time.Hour)
-	
-	go func() {
-		for _ = range tickers[0].C {
-			notarize()
-		}
-	}()
-	
+
 	go func() {
 		for _ = range tickers[1].C {
 			save()
@@ -89,41 +97,85 @@ func init() {
 	}()
 }
 
+
+
 func main() {
-	flag.Parse()
+
+	cadr, err := btcutil.DecodeAddress("mtmkwxRDToguqs6q9dX526wq8xxh8TCbGB", activeNet.Params)
+
+	currentAddr = &cadr
+
+	// Only override the handlers for notifications you care about.
+	// Also note most of the handlers will only be called if you register
+	// for notifications.  See the documentation of the btcrpcclient
+	// NotificationHandlers type for more details about each handler.
+	ntfnHandlers := btcrpcclient.NotificationHandlers{
+		OnAccountBalance: func(account string, balance btcutil.Amount, confirmed bool) {
+		     go newBalance(account, balance, confirmed)
+	    },
+
+		OnBlockConnected: func(hash *btcwire.ShaHash, height int32) {
+			go newBlock(hash, height)
+		},
+	}
 	
+	// Connect to local btcwallet RPC server using websockets.
+	certHomeDir := btcutil.AppDataDir("btcwallet", false)
+	certs, err := ioutil.ReadFile(filepath.Join(certHomeDir, "rpc.cert"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	connCfg := &btcrpcclient.ConnConfig{
+		Host:         "localhost:18332",
+		Endpoint:     "ws",
+		User:         "testuser",
+		Pass:         "notarychain",
+		Certificates: certs,
+	}
+	
+	client, err = btcrpcclient.New(connCfg, &ntfnHandlers)
+	
+	if err != nil {
+		log.Fatal(err)
+		return;
+	}else{
+        defer shutdown(client) 
+    }  		
+	
+	
+	flag.Parse()
+
 	defer func() {
 		tickers[0].Stop()
 		tickers[1].Stop()
 		save()
 		dynrsrc.Stop()
 	}()
-	
-	http.HandleFunc("/", serveRESTfulHTTP)
-	err := http.ListenAndServe(":" + strconv.Itoa(*portNumber), nil)
-	if err != nil { panic(err) }
-}
 
-func notarize() {
-//	fmt.Println("Checking if should send current block")
-	blockMutex.Lock()
-//	fmt.Println("Sending block, creating new block")
-	blockMutex.Unlock()
+	http.HandleFunc("/", serveRESTfulHTTP)
+	err = http.ListenAndServe(":"+strconv.Itoa(*portNumber), nil)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func save() {
 	bcp := make([]*notaryapi.Block, len(blocks))
-	
+
 	blockMutex.Lock()
 	copy(bcp, blocks)
 	blockMutex.Unlock()
-	
+
 	for i, block := range bcp {
 		data, err := block.MarshalBinary()
-		if err != nil { panic(err) }
-		
+		if err != nil {
+			panic(err)
+		}
+
 		err = ioutil.WriteFile(fmt.Sprintf(`app/rest/store.%d.block`, i), data, 0777)
-		if err != nil { panic(err) }
+		if err != nil {
+			panic(err)
+		}
 	}
 }
 
@@ -131,27 +183,27 @@ func serveRESTfulHTTP(w http.ResponseWriter, r *http.Request) {
 	var resource interface{}
 	var err *notaryapi.Error
 	var buf bytes.Buffer
-	
+
 	path, method, accept, form, err := parse(r)
-	
+
 	defer func() {
 		switch accept {
 		case "text":
 			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-			
+
 		case "json":
 			w.Header().Set("Content-Type", "application/json; charset=utf-8")
-			
+
 		case "xml":
 			w.Header().Set("Content-Type", "application/xml; charset=utf-8")
-			
+
 		case "html":
 			w.Header().Set("Content-Type", "text/html; charset=utf-8")
 		}
-	
+
 		if err != nil {
 			var r *notaryapi.Error
-			
+
 			buf.Reset()
 			r = notaryapi.Marshal(err, accept, &buf, false)
 			if r != nil {
@@ -159,32 +211,32 @@ func serveRESTfulHTTP(w http.ResponseWriter, r *http.Request) {
 			}
 			w.WriteHeader(err.HTTPCode)
 		}
-		
+
 		buf.WriteTo(w)
 		w.Write([]byte("\n\n"))
 	}()
-	
+
 	switch method {
 	case "GET":
 		resource, err = find(path)
-		
+
 	case "POST":
 		if len(path) != 1 {
 			err = notaryapi.CreateError(notaryapi.ErrorBadMethod, `POST can only be used in the root context: /v1`)
 			return
 		}
-		
-		resource, err = post("/" + strings.Join(path, "/"), form)
-		
+
+		resource, err = post("/"+strings.Join(path, "/"), form)
+
 	default:
 		err = notaryapi.CreateError(notaryapi.ErrorBadMethod, fmt.Sprintf(`The HTTP %s method is not supported`, method))
 		return
 	}
-	
+
 	if err != nil {
 		resource = err
 	}
-	
+
 	alt := false
 	for _, s := range form["byref"] {
 		b, err := strconv.ParseBool(s)
@@ -193,7 +245,7 @@ func serveRESTfulHTTP(w http.ResponseWriter, r *http.Request) {
 			break
 		}
 	}
-	
+
 	err = notaryapi.Marshal(resource, accept, &buf, alt)
 }
 
@@ -202,7 +254,7 @@ var blockPtrType = reflect.TypeOf((*notaryapi.Block)(nil)).Elem()
 func post(context string, form url.Values) (interface{}, *notaryapi.Error) {
 	newEntry := notaryapi.NewDataEntry([]byte{})
 	format, data := form.Get("format"), form.Get("data")
-	
+
 	switch format {
 	case "", "json":
 		reader := gocoding.ReadString(data)
@@ -210,30 +262,30 @@ func post(context string, form url.Values) (interface{}, *notaryapi.Error) {
 		if err != nil {
 			return nil, notaryapi.CreateError(notaryapi.ErrorJSONUnmarshal, err.Error())
 		}
-		
+
 	case "xml":
 		err := xml.Unmarshal([]byte(data), newEntry)
 		if err != nil {
 			return nil, notaryapi.CreateError(notaryapi.ErrorXMLUnmarshal, err.Error())
 		}
-	
+
 	default:
 		return nil, notaryapi.CreateError(notaryapi.ErrorUnsupportedUnmarshal, fmt.Sprintf(`The format "%s" is not supported`, format))
 	}
-	
+
 	if newEntry == nil {
 		return nil, notaryapi.CreateError(notaryapi.ErrorInternal, `Entity to be POSTed is nil`)
 	}
-	
+
 	newEntry.StampTime()
-	
+
 	blockMutex.Lock()
-	err := blocks[len(blocks) - 1].AddEntry(newEntry)
+	err := blocks[len(blocks)-1].AddEntry(newEntry)
 	blockMutex.Unlock()
-	
+
 	if err != nil {
 		return nil, notaryapi.CreateError(notaryapi.ErrorInternal, fmt.Sprintf(`Error while adding Entity to Block: %s`, err.Error()))
 	}
-	
+
 	return newEntry, nil
 }
