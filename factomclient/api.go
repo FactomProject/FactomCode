98c69939704e1badcd4d043d037451f066eb34b9
mjb <michaeljbeam@gmail.com>
2014-11-16 19:23:15 -0600

    added functions to api.go

--- factomclient/api.go
+++ factomclient/api.go
@@ -1,8 +1,55 @@
 package factomclient
 
 import (
+	"net/http"
+	"net/url"
+	"encoding/hex"	
 
-
-
+	"github.com/FactomProject/FactomCode/noteryapi"
 )
 
+//for now just set the server statically
+var server string = "http://demo.factom.org/v1"
+
+func CreateChain(name [][]byte) (*Hash, error) {
+	c := new(notaryapi.Chain)
+	c.Name := name	
+	c.GenerateIDFromName()
+	return c.ChainID, nil
+}
+
+func SubmitChain(c *notaryapi.Chain) error {
+	bin := c.MarshalBinary()
+	
+	data := url.Values	
+	data.Set("datatype", "chain")
+	data.Set("format", "binary")
+	data.Set("chain", hex.EncodeToString(&bin))
+	
+	resp, err := http.PostForm(server, data)
+	if err != nil {
+		return err
+	}
+}
+
+func CreateEntry(cid *Hash) (*notaryapi.Entry, error) {
+	e := new(notaryapi.Entry)
+	e.ChainID := cid
+	e.ExtHashes := h
+	e.Data := data
+	
+	return e
+}
+
+func SubmitEntry(e *noteryapi.Entry) error {
+	bin := e.MarshalBinary()
+
+	data := url.Values
+	data.Set("format", "binary")
+	data.Set("entry", hex.EncodeToString(&bin))
+
+	resp, err := http.PostForm(server, data)
+	if err != nil {
+		return err
+	}
+}
