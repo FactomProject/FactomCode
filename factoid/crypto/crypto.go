42b6d297d33d5970e3a73f6629c14a8dc03ca447
SirHalsell <sir.halsell@gmail.com>
2014-12-14 22:47:47 -0600

    add support for factoid, key and address management etc

---
+++ factoid/crypto/crypto.go
@@ -0,0 +1,19 @@
+package crypto
+
+import (
+	//"code.google.com/p/go.crypto/sha3"
+	"github.com/FactomProject/FactomCode/factoid/util"
+	"github.com/obscuren/sha3"
+)
+
+func Sha3Bin(data []byte) []byte {
+	d := sha3.NewKeccak256()
+	d.Write(data)
+
+	return d.Sum(nil)
+}
+
+// Creates an ethereum address given the bytes and the nonce
+func CreateAddress(b []byte, nonce uint64) []byte {
+	return Sha3Bin(util.NewValue([]interface{}{b, nonce}).Encode())[12:]
+}
