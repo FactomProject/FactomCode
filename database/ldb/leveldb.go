344586cecffd9ee68bf95211690bba172c9848a6
JackXLu <jack.xujun.lu@gmail.com>
2015-01-21 15:18:01 -0600

    Fix the conformal reference issue #122

--- database/ldb/leveldb.go
+++ database/ldb/leveldb.go
@@ -10,7 +10,7 @@ import (
 
 	"github.com/FactomProject/FactomCode/database"
 
-	"github.com/conformal/btcwire"
+	"github.com/btcsuite/btcwire"
 	"github.com/FactomProject/goleveldb/leveldb"
 	"github.com/FactomProject/goleveldb/leveldb/cache"
 	"github.com/FactomProject/goleveldb/leveldb/opt"
