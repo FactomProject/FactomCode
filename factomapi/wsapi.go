40d3bce04f542e49703fb9843de3ed78cde659fb
michaelbeam <michaeljbeam@gmail.com>
2015-01-26 13:02:40 -0600

    formatting on factomapi/wsapi.go

--- factomapi/wsapi.go
+++ factomapi/wsapi.go
@@ -1,19 +1,17 @@
 package factomapi
 
 import (
-	"io"
+	"errors"
+	"fmt"
+	"github.com/FactomProject/FactomCode/notaryapi"
 	"github.com/FactomProject/gocoding"
-	"github.com/FactomProject/gocoding/json"
 	"github.com/FactomProject/gocoding/html"
-	"github.com/FactomProject/FactomCode/notaryapi"	
-	"fmt"	
-	"errors"	
+	"github.com/FactomProject/gocoding/json"
+	"io"
 )
 
-
-
 func SafeMarshal(writer io.Writer, obj interface{}) error {
-	marshaller := json.NewMarshaller()	
+	marshaller := json.NewMarshaller()
 	renderer := json.Render(writer)
 	renderer.SetRecoverHandler(safeRecover)
 	return marshaller.Marshal(renderer, obj)
@@ -22,22 +20,23 @@ func SafeMarshal(writer io.Writer, obj interface{}) error {
 func SafeMarshalHTML(writer io.Writer, obj interface{}) error {
 	renderer := html.Render(writer)
 	renderer.SetRecoverHandler(safeRecover)
-	
-	marshallerHTML := html.NewMarshaller()	
+
+	marshallerHTML := html.NewMarshaller()
 	return marshallerHTML.Marshal(renderer, obj)
 }
 
 func SafeUnmarshal(reader gocoding.SliceableRuneReader, obj interface{}) error {
 	scanner := json.Scan(reader)
 	scanner.SetRecoverHandler(EXPLODE)
-	unmarshaller := notaryapi.NewJSONUnmarshaller()	
+	unmarshaller := notaryapi.NewJSONUnmarshaller()
 	return unmarshaller.Unmarshal(scanner, obj)
 }
+
 func safeRecover(obj interface{}) error {
 	if err, ok := obj.(error); ok {
 		return err
 	}
-	
+
 	return errors.New(fmt.Sprint(obj))
 }
 
