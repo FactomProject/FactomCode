b49399dca7e6270b0052bbf44d45be26d65f0f85
michaelbeam <michaeljbeam@gmail.com>
2015-02-26 14:21:37 -0600

    log.go updates and simplification

--- factomclient/log.go
+++ factomclient/log.go
@@ -1,12 +1,14 @@
 package factomclient
 
 import (
-	"github.com/FactomProject/FactomCode/factomlog"
 	"os"
+
+	"github.com/FactomProject/FactomCode/factomlog"
 )
 
+var logfile, _ = os.OpenFile(logPath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0660)
+
 var (
-	logfile, _ = os.OpenFile(logPath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0660)
 	rpcLog     = factomlog.New(logfile, logLevel, "rpc")
 	serverLog  = factomlog.New(logfile, logLevel, "serv")
 )
