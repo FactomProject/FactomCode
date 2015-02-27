ed673494684f84bc69928b1f0c10996824e771bc
michaelbeam <michaeljbeam@gmail.com>
2015-02-27 12:12:40 -0600

    updated test

--- factomclient/log_test.go
+++ factomclient/log_test.go
@@ -12,7 +12,7 @@ var _ = fmt.Sprint("")
 var _ = os.DevNull
 
 func TestLoadConfigurations(t *testing.T) {
-	cfg := util.GetConfig()
+	cfg := util.ReadConfig()
 	fmt.Printf("%v\n", cfg)
 	fmt.Println("LogLevel =", cfg.Log.LogLevel)
 	fmt.Println("LogPath =", cfg.Log.LogPath)
