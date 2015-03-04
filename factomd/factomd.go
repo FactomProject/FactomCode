55e996b85b4f1939a2dc47e5fae50011beec2ee5
Michael <michael@mastercoin.org>
2015-03-04 10:09:03 -0500

    removed btcd stuff from factomd dir

--- factomd/factomd.go
+++ factomd/factomd.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
-package main
+package factomd
 
 import (
 	"fmt"
@@ -39,138 +39,7 @@ var (
 // as a service and reacts accordingly.
 var winServiceMain func() (bool, error)
 
-// factomdMain is the real main function for btcd.  It is necessary to work around
-// the fact that deferred functions do not run when os.Exit() is called.  The
-// optional serverChan parameter is mainly used by the service code to be
-// notified with the server once it is setup so it can gracefully stop it when
-// requested from the service control manager.
-func factomdMain(serverChan chan<- *server) error {
-	// Load configuration and parse command line.  This function also
-	// initializes logging and configures it accordingly.
-	/*	tcfg, _, err := loadConfig()
-		if err != nil {
-			return err
-		}
-		cfg = tcfg
-		defer backendLog.Flush()
-
-		// Show version at startup.
-		fmt.Printf("Version %s\n", version())
-
-		// Enable http profiling server if requested.
-		if cfg.Profile != "" {
-			go func() {
-				listenAddr := net.JoinHostPort("", cfg.Profile)
-				fmt.Printf("Profile server listening on %s\n", listenAddr)
-				profileRedirect := http.RedirectHandler("/debug/pprof",
-					http.StatusSeeOther)
-				http.Handle("/", profileRedirect)
-				fmt.Printf("%v\n", http.ListenAndServe(listenAddr, nil))
-			}()
-		}
-
-		// Write cpu profile if requested.
-		if cfg.CPUProfile != "" {
-			f, err := os.Create(cfg.CPUProfile)
-			if err != nil {
-				fmt.Printf("Unable to create cpu profile: %v\n", err)
-				return err
-			}
-			pprof.StartCPUProfile(f)
-			defer f.Close()
-			defer pprof.StopCPUProfile()
-		}
-
-		// Perform upgrades to btcd as new versions require it.
-		if err := doUpgrades(); err != nil {
-			fmt.Printf("%v\n", err)
-			return err
-		}
-
-		// Load the block database.
-		db, err := loadBlockDB()
-		if err != nil {
-			fmt.Printf("%v\n", err)
-			return err
-		}
-		defer db.Close()
-
-		// Ensure the database is sync'd and closed on Ctrl+C.
-		addInterruptHandler(func() {
-			fmt.Printf("Gracefully shutting down the database...")
-			db.RollbackClose()
-		})
-	*/
-
-	cfg_Listeners := []string{"0.0.0.0:4012"}
-
-	// Create server and start it.
-	server, err := newServer(cfg_Listeners, activeNetParams.Params)
-	if err != nil {
-		// TODO(oga) this logging could do with some beautifying.
-		fmt.Printf("Unable to start server on %v: %v\n",
-			cfg_Listeners, err)
-		return err
-	}
-	addInterruptHandler(func() {
-		fmt.Printf("Gracefully shutting down the server...")
-		server.Stop()
-		server.WaitForShutdown()
-	})
-	server.Start()
-	if serverChan != nil {
-		serverChan <- server
-	}
-
-	// Write outgoing factom messages into P2P network
-	go func() {
-		for msg := range outMsgQueue {
-			server.BroadcastMessage(msg)
-			/*			peerInfoResults := server.PeerInfo()
-						for peerInfo := range peerInfoResults{
-							fmt.Printf("PeerInfo:%+v", peerInfo)
-
-						}*/
-		}
-	}()
-
-	go func() {
-		for msg := range inRpcQueue {
-			fmt.Printf("in range inRpcQueue, msg:%+v\n", msg)
-			switch msg.Command() {
-			case factomwire.CmdTx:
-				inMsgQueue <- msg	//		for testing
-				server.blockManager.QueueTx(msg.(*factomwire.MsgTx), nil)
-			case factomwire.CmdConfirmation:
-				server.blockManager.QueueConf(msg.(*factomwire.MsgConfirmation), nil)
-
-			default:
-				inMsgQueue <- msg
-				outMsgQueue <- msg
-			}
-		}
-	}()
-
-	// Monitor for graceful server shutdown and signal the main goroutine
-	// when done.  This is done in a separate goroutine rather than waiting
-	// directly so the main goroutine can be signaled for shutdown by either
-	// a graceful shutdown or from the main interrupt handler.  This is
-	// necessary since the main goroutine must be kept running long enough
-	// for the interrupt handler goroutine to finish.
-	go func() {
-		server.WaitForShutdown()
-		fmt.Println("Server shutdown complete")
-		shutdownChannel <- struct{}{}
-	}()
-
-	// Wait for shutdown signal from either a graceful server stop or from
-	// the interrupt handler.
-	<-shutdownChannel
-	fmt.Println("Shutdown complete")
-	return nil
-}
-
-func main() {
+func Factomd_main() {
 
 	util.Trace()
 	// Use all processor cores.
@@ -198,15 +67,17 @@ func main() {
 	*/
 	util.Trace()
 
-	// Work around defer not working after os.Exit()
-	if err := factomdMain(nil); err != nil {
-		os.Exit(1)
-	}
+	/*
+		// Work around defer not working after os.Exit()
+		if err := factomdMain(nil); err != nil {
+			os.Exit(1)
+		}
+	*/
 
 	util.Trace()
 }
 
-func init() {
+func Factomd_init() {
 
 	util.Trace()
 
