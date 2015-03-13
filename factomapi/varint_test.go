bb6f0e54350b8e5a468130081cf483b87e05d1bc
michaelbeam <michaeljbeam@gmail.com>
2015-03-13 04:33:41 -0500

    Read and Write varint are working

--- factomapi/varint_test.go
+++ factomapi/varint_test.go
@@ -1,3 +1,7 @@
+// Copyright 2015 Factom Foundation
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
 package factomapi
 
 import (
@@ -39,10 +43,21 @@ func TestWriteVarint(t *testing.T) {
 }
 
 func TestReadVarint(t *testing.T) {
-	var num uint64 = 1e9
+	var (
+		num1 uint64 = 1e9
+		num2 uint64 = 5
+	)
+	
 	buf := new(bytes.Buffer)
 
-	n, err := WriteVarint(buf, num)
+	n, err := WriteVarint(buf, num1)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	fmt.Println("n =", n)
+	fmt.Println("buf =", hex.EncodeToString(buf.Bytes()))
+
+	n, err = WriteVarint(buf, num2)
 	if err != nil {
 		t.Errorf(err.Error())
 	}
@@ -50,6 +65,13 @@ func TestReadVarint(t *testing.T) {
 	fmt.Println("buf =", hex.EncodeToString(buf.Bytes()))
 	
 	x, err := ReadVarint(buf)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	fmt.Println("x =", x)
+	fmt.Println("buf =", hex.EncodeToString(buf.Bytes()))
+
+	x, err = ReadVarint(buf)
 	if err != nil {
 		t.Errorf(err.Error())
 	}
