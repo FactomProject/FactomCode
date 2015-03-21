fc742d81d98523e65be8ee9025e2282510de625e
JackXLu <jack.xujun.lu@gmail.com>
2015-03-21 16:28:27 -0500

    Additional PL methods

--- restapi/mempool.go
+++ restapi/mempool.go
@@ -1,16 +1,32 @@
 package restapi
 
 import (
+	"github.com/FactomProject/btcd/wire"
 	"sync"
 	"time"
-	"github.com/FactomProject/btcd/wire"	
 )
 
-// ftmMemPool is used as a source of factom transactions 
+// ftmMemPool is used as a source of factom transactions
 // (CommitChain, RevealChain, CommitEntry, RevealEntry)
 type ftmMemPool struct {
 	sync.RWMutex
-	pool          map[wire.ShaHash]wire.Message
-	orphans       map[wire.ShaHash]wire.Message
-	lastUpdated   time.Time // last time pool was updated
+	pool        map[wire.ShaHash]wire.Message
+	orphans     map[wire.ShaHash]wire.Message
+	lastUpdated time.Time // last time pool was updated
+}
+
+// Add a factom message to the  Mem pool
+func (mp *ftmMemPool) addMsg(msg wire.Message, hash *wire.ShaHash) error {
+
+	mp.pool[*hash] = msg
+
+	return nil
+}
+
+// Add a factom message to the orphan pool
+func (mp *ftmMemPool) addOrphanMsg(msg wire.Message, hash *wire.ShaHash) error {
+
+	mp.orphans[*hash] = msg
+
+	return nil
 }
