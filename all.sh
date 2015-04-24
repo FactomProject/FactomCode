e4c66a4e947c1b44c6b5f4c90cc2b79728156a9e
Paul Snow <snow.paul@gmail.com>
2015-04-23 20:46:26 -0500

    Helper script to get up to date

---
+++ all.sh
@@ -0,0 +1,97 @@
+cd ..
+cd btcd
+git checkout development
+git pull
+cd ..
+cd btcjson
+git checkout development
+git pull
+cd ..
+cd btclog 
+git checkout development
+git pull
+cd ..
+cd btcrpcclient
+git checkout development
+git pull
+cd ..
+cd btcutil
+git checkout development
+git pull
+cd ..
+cd btcwallet
+git checkout development
+git pull
+cd ..
+cd btcws 
+git checkout development
+git pull
+cd ..
+cd dynrsrc
+git checkout development
+git pull
+cd ..
+cd factom
+git checkout development
+git pull
+cd ..
+cd factom-cli 
+git checkout development
+git pull
+cd ..
+cd FactomDocs
+git checkout development
+git pull
+cd ..
+cd factomexplorer 
+git checkout development
+git pull
+cd ..
+cd fastsha256 
+git checkout development
+git pull
+cd ..
+cd gobundle 
+git checkout development
+git pull
+cd ..
+cd gocoding 
+git checkout master
+git pull
+cd ..
+cd go-flags 
+git checkout development
+git pull
+cd ..
+cd goleveldb 
+git checkout development
+git pull
+cd ..
+cd go-socks 
+git checkout development
+git pull
+cd ..
+cd seelog 
+git checkout development
+git pull
+cd ..
+cd snappy-go 
+git checkout development
+git pull
+cd ..
+cd website 
+git checkout development
+git pull
+cd ..
+cd websocket 
+git checkout development
+git pull
+cd ..
+cd WorkItems
+git checkout development
+git pull
+cd ..
+cd FactomCode
+git checkout development
+git pull
+
