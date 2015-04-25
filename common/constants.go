2fe777f096d89e029058960ed949c63b7e962cd1
JackXLu <jack.xujun.lu@gmail.com>
2015-04-25 15:11:42 -0500

    Added admin block definition

--- common/constants.go
+++ common/constants.go
@@ -15,7 +15,8 @@ const (
    
     //Limits and Sizes
     MAX_ENTRY_SIZE  = uint16(10240)      //Maximum size for Entry External IDs and the Data
-    HASH_LENGTH     = int(32)            //Length of a Hash 
+    HASH_LENGTH     = uint64(32)            //Length of a Hash 
+    SIG_LENGTH     = uint64(64)            //Length of a signature     
 	//Common constants
 	VERSION_0       = byte(0)
 	NETWORK_ID_DB   = uint32(4203931041) //0xFA92E5A1	
@@ -27,14 +28,29 @@ const (
 )
 
 //---------------------------------------------------------------
-// Three types of entries (transactions) for Entry Credit Block
+// Types of entries (transactions) for Entry Credit Block
 //---------------------------------------------------------------
 const (
     TYPE_SERVER_INDEX uint8 = iota
     TYPE_MINUTE_NUMBER 
     TYPE_PAY_CHAIN
     TYPE_PAY_ENTRY  
-    TYPE_BUY    
+    TYPE_BUY     
+)
+
+//---------------------------------------------------------------
+// Types of entries (transactions) for Admin Block
+//---------------------------------------------------------------
+const (
+    TYPE_MINUTE_NUM uint8 = iota
+    TYPE_DB_SIGNATURE
+    TYPE_REVEAL_MATRYOSHKA  
+    TYPE_ADD_MATRYOSHKA
+    TYPE_ADD_SERVER_COUNT
+    TYPE_ADD_FED_SERVER
+    TYPE_REMOVE_FED_SERVER
+    TYPE_ADD_FED_SERVER_KEY
+    TYPE_ADD_BTC_ANCHOR_KEY //8     
 )
 
 // Chain Values.  Not exactly constants, but nice to have.
