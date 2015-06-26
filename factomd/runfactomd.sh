9bd657733d8de1349b3af0b7e8fb3234ede17558
Michael <michael@mastercoin.org>
2015-06-26 12:55:31 -0400

    simple redirect scrpit for the server

---
+++ factomd/runfactomd.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#
+# To aid with server debugging, save stdout & stderr to the log file.
+# 
+# Please run as:
+#   runfactomd.sh &
+#
+mkdir ~/logs/ 2> /dev/null
+factomd --debuglevel=debug 2>&1 | tee -a ~/logs/factomd_${RANDOM}_${RANDOM}.log
