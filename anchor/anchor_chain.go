f70edbd35eebdf1d90b858a080f3bf8b03453357
Sir.Halsell <Sir.Halsell@gmail.com>
2016-05-12 23:08:17 -0500

    add debug info to anchor chain

--- anchor/anchor_chain.go
+++ anchor/anchor_chain.go
@@ -14,9 +14,11 @@ import (
 	"encoding/json"
 	"time"
 
+	"github.com/davecgh/go-spew/spew"
+
 	"github.com/FactomProject/FactomCode/common"
 	"github.com/FactomProject/FactomCode/util"
-	factomwire "github.com/FactomProject/FactomCode/wire"
+	"github.com/FactomProject/FactomCode/wire"
 )
 
 //Construct the entry and submit it to the server
@@ -26,22 +28,26 @@ func submitEntryToAnchorChain(aRecord *AnchorRecord) error {
 	jsonARecord, err := json.Marshal(aRecord)
 	//anchorLog.Debug("submitEntryToAnchorChain - jsonARecord: ", string(jsonARecord))
 	if err != nil {
+		anchorLog.Error("submitEntryToAnchorChain: error in json.Marshal(aRecord). " + err.Error())
 		return err
 	}
 	bufARecord := new(bytes.Buffer)
 	bufARecord.Write(jsonARecord)
 	//Sign the json aRecord with the server key
 	aRecordSig := serverPrivKey.Sign(jsonARecord)
+	//Encode sig into Hex string
+	// bufARecord.Write([]byte(hex.EncodeToString(aRecordSig.Sig[:])))
 
 	//Create a new entry
 	entry := common.NewEntry()
 	entry.ChainID = anchorChainID
-	anchorLog.Debug("anchorChainID: ", anchorChainID)
+	// anchorLog.Debug("anchorChainID: ", anchorChainID)
+
 	// instead of append signature at the end of anchor record
 	// it can be added as the first entry.ExtIDs[0]
 	entry.ExtIDs = append(entry.ExtIDs, []byte(aRecordSig.Sig[:]))
 	entry.Content = bufARecord.Bytes()
-	//anchorLog.Debug("entry: ", spew.Sdump(entry))
+	anchorLog.Debug("entry: ", spew.Sdump(entry))
 
 	buf := new(bytes.Buffer)
 	// 1 byte version
@@ -53,11 +59,13 @@ func submitEntryToAnchorChain(aRecord *AnchorRecord) error {
 	// 1 byte number of entry credits to pay
 	binaryEntry, err := entry.MarshalBinary()
 	if err != nil {
+		anchorLog.Error("submitEntryToAnchorChain: error in entry.MarshalBinary. " + err.Error())
 		return err
 	}
 
 	anchorLog.Debug("jsonARecord binary entry: ", hex.EncodeToString(binaryEntry))
 	if c, err := util.EntryCost(binaryEntry); err != nil {
+		anchorLog.Error("submitEntryToAnchorChain: error in util.EntryCost. " + err.Error())
 		return err
 	} else {
 		buf.WriteByte(byte(c))
@@ -72,18 +80,22 @@ func submitEntryToAnchorChain(aRecord *AnchorRecord) error {
 	commit := common.NewCommitEntry()
 	err = commit.UnmarshalBinary(buf.Bytes())
 	if err != nil {
+		anchorLog.Error("submitEntryToAnchorChain: error in commit.UnmarshalBinary. " + err.Error())
 		return err
 	}
+	anchorLog.Debug("CommitEntry: ", spew.Sdump(commit))
 
 	// create a CommitEntry msg and send it to the local inmsgQ
-	cm := factomwire.NewMsgCommitEntry()
+	cm := wire.NewMsgCommitEntry()
 	cm.CommitEntry = commit
 	inMsgQ <- cm
+	anchorLog.Debug("MsgCommitEntry: ", spew.Sdump(cm))
 
 	// create a RevealEntry msg and send it to the local inmsgQ
-	rm := factomwire.NewMsgRevealEntry()
+	rm := wire.NewMsgRevealEntry()
 	rm.Entry = entry
 	inMsgQ <- rm
+	anchorLog.Debug("MsgRevealEntry: ", spew.Sdump(rm))
 
 	return nil
 }
